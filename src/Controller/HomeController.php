ea6db99f3862b3cc9c5f5790edbf17734d4918a3
benjaminroche <benjamin.roche4@gmail.com>
2024-09-01 16:20:06 +0200

    feat: add many changes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,14 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\BlogPostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
+    //construct BlogPost
+    public function __construct(
+        private readonly BlogPostRepository $blogPostRepository
+    )
+    {
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');}
+        return $this->render('home/index.html.twig',[
+            'posts' => $this->blogPostRepository->findLastPublishedPosts()
+        ]);}
 }
--- src/Repository/BlogPostRepository.php
+++ src/Repository/BlogPostRepository.php
@@ -34,4 +34,20 @@ class BlogPostRepository extends ServiceEntityRepository
             ->getQuery()
             ->getResult();
     }
+
+    /**
+     * Find the last 3 posts that were published.
+     *
+     * @param int $limit
+     * @return array
+     */
+    public function findLastPublishedPosts(int $limit = 3): array
+    {
+        return $this->createQueryBuilder('p')
+            ->where('p.published = true')
+            ->orderBy('p.createdAt', 'DESC')
+            ->setMaxResults($limit)
+            ->getQuery()
+            ->getResult();
+    }
 }
